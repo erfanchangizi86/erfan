393ccab5b5fb02a09b95dfadfc64cc1068553af6
erfanchangizi86 <erfanchangizi401@gmail.com>
2024-07-06 11:47:49 +0330

    test

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 AUTH_USER_MODEL = "account.User"
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'erfan',
+        'USER': 'erfan',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'PASSWORD': 'erfan',
     }
 }
 
